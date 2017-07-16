9d67d90c57c12c2a4a1a0298daa4045b706982ea
Kiss Aron <aron.kiss01@gmail.com>
2017-07-16 16:51:06 +0200

    Add Car class

---
+++ TravelRecord/TravelRecord/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace TravelRecord
+{
+    class Car
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; }
+
+        public int CompanyID { get; set; }
+
+        public string CarModel { get; set; }
+
+        public string LicensePlateNumber { get; set; }
+    }
+}
