0cc574337af8d6fcbaee9898a11570506557ebdb
Kiss Aron <aron.kiss01@gmail.com>
2017-07-16 16:54:36 +0200

    Add Travel class

---
+++ TravelRecord/TravelRecord/Travel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace TravelRecord
+{
+    class Travel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; }
+
+        public DateTime Date { get; set; }
+
+        public string StartPoint { get; set; }
+
+        public string Destination { get; set; }
+
+        public int Distance { get; set; } //in kilometers
+    }
+}
