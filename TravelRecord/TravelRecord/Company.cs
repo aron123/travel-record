832bc961f8c15258eb2c1450f70892e4acb4f8e8
Kiss Aron <aron.kiss01@gmail.com>
2017-07-16 16:50:28 +0200

    Add Company class

---
+++ TravelRecord/TravelRecord/Company.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace TravelRecord
+{
+    public class Company
+    {
+        // In current version of this program, only 1 company is enabled.
+        [PrimaryKey]
+        public int ID { get; } = 0; // equal with Car.CompanyID
+
+        public string CompanyName { get; set; }
+
+        public string Address { get; set; }
+
+        public string VATNumber { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("[ Person: ID={0}, CompanyName={1}, Address={2}, VATNumber={3} ]", ID, CompanyName, Address, VATNumber);
+        }
+
+    }
+}
