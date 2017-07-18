3ee1da982345c339dce9ea2a63d2573975a74b07
Kiss Aron <aron.kiss01@gmail.com>
2017-07-18 16:02:01 +0200

    Add SetMainPage method to App class

--- TravelRecord/TravelRecord/App.xaml.cs
+++ TravelRecord/TravelRecord/App.xaml.cs
@@ -12,23 +12,48 @@ namespace TravelRecord
         public App()
         {
             InitializeComponent();
-
-            MainPage = new NavigationPage(new TravelRecord.AddCompanyData());
+            SetMainPage();
+            
         }
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            // Handle when app starts
+            SetMainPage();
+
         }
 
         protected override void OnSleep()
         {
-            // Handle when your app sleeps
+            // Handle when app sleeps
         }
 
         protected override void OnResume()
         {
-            // Handle when your app resumes
+            // Handle when app resumes
+            SetMainPage();
+        }
+
+        public bool IsCompanyDataSet()
+        {
+            if (Application.Current.Properties.ContainsKey("CompanyName")
+                && Application.Current.Properties.ContainsKey("CompanyAddress")
+                && Application.Current.Properties.ContainsKey("CompanyVAT"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Set the main page of the application depending on company's data is set or unset.
+        /// </summary>
+        public void SetMainPage()
+        {
+            //TODO: Change MainPage to travel records' page
+            MainPage = IsCompanyDataSet() ? new NavigationPage(new TravelRecord.MainPage()) : new NavigationPage(new TravelRecord.AddCompanyData());
         }
     }
 }
