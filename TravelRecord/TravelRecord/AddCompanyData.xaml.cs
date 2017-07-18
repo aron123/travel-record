0bf9f78c0820c21240208c7050a83e80e4e9f6b5
Kiss Aron <aron.kiss01@gmail.com>
2017-07-18 16:01:06 +0200

    Implement Button_SaveCompanyData and add debug button with handler

--- TravelRecord/TravelRecord/AddCompanyData.xaml.cs
+++ TravelRecord/TravelRecord/AddCompanyData.xaml.cs
@@ -12,15 +12,35 @@ namespace TravelRecord
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class AddCompanyData : ContentPage
 	{
-		public AddCompanyData ()
+		public AddCompanyData()
 		{
             Title = "Cégadatok";
-			InitializeComponent ();
+			InitializeComponent();
 		}
 
-        private void Button_AddCompanyDataToDB(object sender, EventArgs e)
+        private void Button_SaveCompanyData()
         {
-            throw new NotImplementedException();
+            Application.Current.Properties["CompanyName"] = CompanyName.Text;
+            Application.Current.Properties["CompanyAddress"] = CompanyAddress.Text;
+            Application.Current.Properties["CompanyVAT"] = CompanyVAT.Text;
         }
+
+        //FOR DEBUG
+        private void DEBUGButton_CheckData(object sender, EventArgs e)
+        {
+            if (Application.Current.Properties.ContainsKey("CompanyName")
+                && Application.Current.Properties.ContainsKey("CompanyAddress")
+                && Application.Current.Properties.ContainsKey("CompanyVAT"))
+            {
+                var name = Application.Current.Properties["CompanyName"] as string;
+                var address = Application.Current.Properties["CompanyAddress"] as string;
+                var vat = Application.Current.Properties["CompanyVAT"] as string;
+
+                DisplayAlert("Data is", string.Format("Name: {0}, \nAddress: {1}, \nVAT: {2}", name, address, vat), "OK");
+            }
+            else
+                DisplayAlert("NINCS TÁROLT ADAT", "", "ok");
+        }
+        //
     }
 }
