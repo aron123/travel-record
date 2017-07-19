d3c0e140169bc601456d273cf08df6643cb44da5
Kiss Aron <aron.kiss01@gmail.com>
2017-07-19 13:33:40 +0200

    Add Database connection class and add folders to cross-platform project

---
+++ TravelRecord/TravelRecord/IDatabaseConnection.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelRecord
+{
+    public interface IDatabaseConnection
+    {
+        /// <summary>
+        /// Return the proper connection string
+        /// </summary>
+        /// <returns></returns>
+        SQLite.SQLiteConnection DbConnection(string dbName);
+    }
+}
