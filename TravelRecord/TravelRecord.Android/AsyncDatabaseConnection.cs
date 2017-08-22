ec4bf6de902718572341c51e991aa9b9061c69f0
Kiss Aron <aron.kiss01@gmail.com>
2017-08-22 17:11:57 +0200

    Add classes for connect database asynchronous

---
+++ TravelRecord/TravelRecord.Android/AsyncDatabaseConnection.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using TravelRecord.Droid;
+using System.IO;
+using System;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AsyncDatabaseConnection))]
+
+namespace TravelRecord.Droid
+{
+    public class AsyncDatabaseConnection : IAsyncDatabaseConnection
+    {
+        /// <summary>
+        /// Create the Android-specific path for the database file
+        /// </summary>
+        /// <param name="dbName">Name of the SQLite3 database file (e.g. Users.db3)</param>
+        /// <returns>SQLiteConnection to database</returns>
+        public SQLiteAsyncConnection DbConnection(string dbName)
+        {
+            var path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), dbName);
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
---
+++ TravelRecord/TravelRecord/Data/IAsyncDatabaseConnection.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelRecord
+{
+    public interface IAsyncDatabaseConnection
+    {
+        /// <summary>
+        /// Create the proper platform-specific path for database and return an SQLiteAsyncConnection object
+        /// </summary>
+        /// <returns></returns>
+        SQLite.SQLiteAsyncConnection DbConnection(string dbName);
+    }
+}
