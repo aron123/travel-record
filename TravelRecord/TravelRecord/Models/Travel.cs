d764d715391628d91e494966737cd1ee1c3d66a3
Kiss Aron <aron.kiss01@gmail.com>
2017-08-27 12:59:39 +0200

    Extend Travel.cs to use it for data binding

--- TravelRecord/TravelRecord/Models/Travel.cs
+++ TravelRecord/TravelRecord/Models/Travel.cs
@@ -22,5 +22,17 @@ namespace TravelRecord
         {
             return string.Format("[ Travel: ID={0}, CarLicensePlate={1}, TravelDate={2}, StartPoint={3}, Destination={4}, Distance={5} ]", ID, CarLicensePlate, TravelDate.ToString(), StartPoint, Destination, Distance);
         }
+
+        [Ignore]
+        public string Route { get { return StartPoint + " - " + Destination; } }
+        [Ignore]
+        public string DistanceWithUnit { get { return Distance.ToString() + " km"; } }
+
+        // === These properties are used for data binding in ListTravels page ===
+        [Ignore]
+        public string Text { get { return Route; } }
+        [Ignore]
+        public string Detail { get { return TravelDate.ToString("yyyy. MM. dd.") + ", " + DistanceWithUnit; } }
+        // === ===
     }
 }
