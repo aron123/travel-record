7e28f00b900bbc0904e1f1168f21a887dfa374f5
Kiss Aron <aron.kiss01@gmail.com>
2017-08-29 19:11:15 +0200

    Small fixes in AddCompanyData: - Add new constructor for initialization with prefilled entries - Fix some strings in XAML - Add navigation to save button

--- TravelRecord/TravelRecord/Pages/AddCompanyData.xaml.cs
+++ TravelRecord/TravelRecord/Pages/AddCompanyData.xaml.cs
@@ -6,23 +6,42 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using SQLite; //DEBUG
 
 namespace TravelRecord
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class AddCompanyData : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddCompanyData : ContentPage
+    {
+        /// <summary>
+        /// Initialize AddCompanyData with empty entries
+        /// </summary>
 		public AddCompanyData()
 		{
 			InitializeComponent();
 		}
+
+        /// <summary>
+        /// Initialize AddCompanyData page with prefilled entries
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="address"></param>
+        /// <param name="vatnumber"></param>
+        public AddCompanyData(string name, string address, string vatnumber)
+        {
+            InitializeComponent();
+
+            CompanyName.Text = name;
+            CompanyAddress.Text = address;
+            CompanyVAT.Text = vatnumber;
+        }
 
         private void Button_SaveCompanyData()
         {
             Application.Current.Properties["CompanyName"] = CompanyName.Text;
             Application.Current.Properties["CompanyAddress"] = CompanyAddress.Text;
             Application.Current.Properties["CompanyVAT"] = CompanyVAT.Text;
+
+            Navigation.PopAsync();
         }
     }
 }
