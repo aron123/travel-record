9aa8a56b9cccd55eacb8af684566ade254b58885
Kiss Aron <aron.kiss01@gmail.com>
2017-08-30 20:19:17 +0200

    Add Error page

---
+++ TravelRecord/TravelRecord/Pages/Error.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecord
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Error : ContentPage
+    {
+        /// <summary>
+        /// Initialize an Error page with the give title and message.
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="message"></param>
+        public Error(string title, string message)
+        {
+            InitializeComponent();
+            ErrorTitle.Text = title;
+            ErrorMessage.Text = message;
+        }
+    }
+}
