0a84bc23672e319129f840a7822c9380c4fd2455
Kiss Aron <aron.kiss01@gmail.com>
2017-08-30 18:35:31 +0200

    Remove debug method from App

--- TravelRecord/TravelRecord/App.xaml.cs
+++ TravelRecord/TravelRecord/App.xaml.cs
@@ -13,7 +13,6 @@ namespace TravelRecord
         public App()
         {
             InitializeComponent();
-            //DEBUG_InitializeTables();
         }
 
         protected override void OnStart()
@@ -56,7 +55,7 @@ namespace TravelRecord
             if (IsExist == null) return false;
 
             //Check if existing Car table is empty
-            int IsEmpty = database.Query<Car>("SELECT 1 FROM Car LIMIT 1;").Count;
+            int IsEmpty = database.ExecuteScalar<int>("SELECT COUNT(*) FROM Car LIMIT 1;");
             if (IsEmpty == 0) return false;
 
             return true;
@@ -83,66 +82,6 @@ namespace TravelRecord
                 MainPage = new NavigationPage(new AddCompanyData());
             }
 
-        }
-
-        private void DEBUG_InitializeTables()
-        {
-            Car car = new Car() { LicensePlateNumber = "ABC123", CarModel = "Sárga Ferrari" };
-            Car car2 = new Car() { LicensePlateNumber = "XYZ987", CarModel = "Trabant 601" };
-            Travel travel0 = new Travel()
-            {
-                CarLicensePlate = "ABC123",
-                TravelDate = new DateTime(2017, 02, 15),
-                StartPoint = "Encs",
-                Destination = "Miskolc",
-                Distance = 60
-            };
-
-            Travel travel1 = new Travel()
-            {
-                CarLicensePlate = "ABC123",
-                TravelDate = new DateTime(2015, 05, 15),
-                StartPoint = "Miskolc",
-                Destination = "Encs",
-                Distance = 60
-            };
-
-            Travel travel2 = new Travel()
-            {
-                CarLicensePlate = "ABC123",
-                TravelDate = new DateTime(2020, 12, 31),
-                StartPoint = "Budapest",
-                Destination = "Hatvan",
-                Distance = 59
-            };
-
-            Travel travel3 = new Travel()
-            {
-                CarLicensePlate = "ABC123",
-                TravelDate = new DateTime(2007, 05, 06),
-                StartPoint = "Győr",
-                Destination = "Szeged",
-                Distance = 280
-            };
-
-            Travel[] travels = { travel0, travel1, travel2, travel3 };
-
-            SQLiteConnection database;
-            database = DependencyService.Get<IDatabaseConnection>().DbConnection("AppDatabase.db3");
-
-            database.DropTable<Car>();
-            database.DropTable<Travel>();
-
-            database.CreateTable<Car>();
-            database.CreateTable<Travel>();
-
-            database.Insert(car);
-            database.Insert(car2);
-
-            for(int i = 0; i < travels.Length; i++)
-            {
-                database.Insert(travels[i]);
-            }
         }
     }
 }
