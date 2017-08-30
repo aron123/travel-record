4972da305461d9efd9295a1ec9457c6548d779eb
Kiss Aron <aron.kiss01@gmail.com>
2017-08-30 20:21:28 +0200

    Add DatabaseAssistant class

---
+++ TravelRecord/TravelRecord/Data/DatabaseAssistant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Xamarin.Forms;
+
+namespace TravelRecord
+{
+    public static class DatabaseAssistant
+    {
+        static string dbName = "AppDatabase.db3";
+
+        public static SQLiteConnection Connect()
+        {
+            try
+            {
+                return DependencyService.Get<IDatabaseConnection>().DbConnection(dbName);
+            }
+            catch (SQLiteException e)
+            {
+                throw e;
+            }
+        }
+    }
+}
